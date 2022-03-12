fced9f19f59bf7b4484695004a4c66a7d47f4d7e
gylee <gylee@payhere.in>
2022-03-13 00:07:33 +0900

    220313

--- T22_form/form.py
+++ T22_form/form.py
@@ -1,19 +1,26 @@
 """
 http -v -f POST :8000/login username=gy password=1234
-http -v -f POST :8000/login username=gy password=1234 file@images/apple.jpeg
+http -v -f POST :8000/login username=gy password=1234 file@backup/apple.jpeg
 """
 
 from fastapi import FastAPI, Form, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.post("/login")
-def login(username: str = Form(...), password: str = Form(...), file: UploadFile = File(None)):
-    response = {"username": username, "password": password}
+def login(username: str = Form(...),
+          password: str = Form(...),
+          file: UploadFile = File(None)):
+    """
+    파일 전송이 없으면: application/x-www-urlencoded
+    파일 전송이 있으면: multipart/form-data
+    """
+    resp = {"username": username, "password": password}
     if file:
-        response.update({"filename": file.filename, "content": file.content_type})
+        resp.update({"filename": file.filename, "content": file.content_type})
 
-    return response
+    return resp
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-uvicorn api.main:app --host 0.0.0.0 --port 8000
